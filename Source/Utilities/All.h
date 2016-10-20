7565009053c17b26b8ad2abaf86f284a87470384
Convery <convery10122@gmail.com>
2016-10-20 15:07:59 +0200

    Initial commit

---
+++ Source/Utilities/All.h
@@ -0,0 +1,18 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: MIT
+    Started: 2016-10-20
+    Notes:
+        Includes all utility classes.
+*/
+
+#pragma once
+
+#ifndef KEEP_AYRIA_NAMESPACE
+using namespace AYRIA;
+#endif
+
+#include "Crypto/FNV1.h"
+#include "Files/Filesystem.h"
+#include "Strings/Debugstring.h"
+#include "Strings/Variadicstring.h"

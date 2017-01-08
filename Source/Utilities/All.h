3a8168e8cee4a23474d9fe1b695cbe9fc0f7f0f9
Convery <convery10122@gmail.com>
2017-01-08 15:50:13 +0100

    Added FNV1

---
+++ Source/Utilities/All.h
@@ -0,0 +1,11 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: MIT
+    Started: 2017-1-8
+    Notes:
+        Includes all utility files.
+*/
+
+#pragma once
+
+#include "Hash/FNV1.h"
